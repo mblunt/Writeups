580aa7135d8ea7fd0ea6f484b58c10891be27214
mblunt <52963601+mblunt@users.noreply.github.com>
2022-10-12 09:46:33 -0400

    Create crackme.py

---
+++ CTF/PicoCTF/Reversing/crackme.py
@@ -0,0 +1,4 @@
+Flag is hard coded into the program, no need to even rev engineer
+Just take the string and put it into cyberchef rot47, boom flag
+You can also just replace the function at the bottom with decode_secret(bezos_cc_secret) and it will print the flag
+picoCTF{1|\/|_4_p34|\|ut_8c551048}
